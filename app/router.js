efdc45d50773127abddd3fb250104366de458323
AarthiNatesan <aarthinatesan2001@gmail.com>
2022-02-09 01:22:37 +0530

    my second commit

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
     this.route('studentdetails');
+    this.route('newlist');
 });
 
 export default Router;
---
+++ app/routes/newlist.js
@@ -0,0 +1,22 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+    actions: {
+        success() {
+            const reg_num = this.get('reg_num');
+            const first = this.get('first');
+            const last = this.get('last');
+            const ph_num = this.get('ph_num');
+            const year = this.get('year');
+            const city = this.get('city');
+            const newRecord = this.store.createRecord('studentdetails', {
+                registration_num: reg_num,
+                first_name: first,
+                last_name: last,
+                phone_num: ph_num,
+                year: year,
+                city: city
+            })
+        }
+    }
+});
