df0dffeaf44ba8e1accf15da7eadc950bee63080
AarthiNatesan <aarthinatesan2001@gmail.com>
2022-02-10 11:16:43 +0530

    my fourth commit

--- mirage/config.js
+++ mirage/config.js
@@ -6,32 +6,38 @@ export default function() {
             data: [{
                     type: 'studentdetails',
                     id: 1,
-                    registration_num: 1,
-                    first_name: 'Aarthi',
-                    last_name: 'N',
-                    phone_num: 8903841274,
-                    year: 4,
-                    city: 'Namakkal'
+                    attributes: {
+                        registration_num: 1,
+                        first_name: 'Aarthi',
+                        last_name: 'N',
+                        phone_num: 8903841274,
+                        year: 4,
+                        city: 'Namakkal'
+                    }
                 },
                 {
                     type: 'studentdetails',
                     id: 2,
-                    registration_num: 2,
-                    first_name: 'Barani',
-                    last_name: 'R',
-                    phone_num: 947841274,
-                    year: 4,
-                    city: 'Erode'
+                    attributes: {
+                        registration_num: 2,
+                        first_name: 'Barani',
+                        last_name: 'R',
+                        phone_num: 947841274,
+                        year: 4,
+                        city: 'Erode'
+                    }
                 },
                 {
                     type: 'studentdetails',
                     id: 3,
-                    registration_num: 3,
-                    first_name: 'Charu',
-                    last_name: 'L',
-                    phone_num: 76373841274,
-                    year: 4,
-                    city: 'Salem'
+                    attributes: {
+                        registration_num: 3,
+                        first_name: 'Charu',
+                        last_name: 'L',
+                        phone_num: 76373841274,
+                        year: 4,
+                        city: 'Salem'
+                    }
                 }
             ]
         }
