4215d9db0697cab2fc95347c06b908b5ae821ee3
AarthiNatesan <aarthinatesan2001@gmail.com>
2022-02-22 15:38:06 +0530

    my commit

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,3 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
-    openModal(options) {
-        let model = this.modelFor(this.routeName);
-        options.canShowModal = true;
-        Ember.setProperties(model, {
-            modalOptions: options
-        });
-    }
-});
+export default Route.extend({});
